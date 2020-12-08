0d7dee4400ac04a87eba0195744af3ced638aafa
Emetophobe <snapnaw@gmail.com>
2020-12-08 07:30:08 -0500

    Code cleanup. Better exception handling.

--- searchdb.py
+++ searchdb.py
@@ -6,35 +6,8 @@
 import sys
 import argparse
 import requests
+
 from bs4 import BeautifulSoup
-
-
-def search_db(game):
-    """ Search steamdb.info for the game. """
-    # Perform the query using Requests
-    dbsearch = f'https://steamdb.info/search/?a=app&q={game}&type=1&category=0'
-    r = requests.get(dbsearch, headers={'User-Agent': 'Mozilla 5.0/NT'})
-    r.raise_for_status()    # throws a Requests Exception on http error
-
-    # Parse the html using BeautifulSoup
-    soup = BeautifulSoup(r.text, 'html.parser')
-    table = soup.find('table', {'class': 'table table-bordered table-hover'})
-    if not table:
-        print('Found 0 results.')
-        return
-
-    # Create a dictionary of the results
-    results = {}
-    for row in table.find_all('tr'):
-        column = row.text.strip().split('\n')
-        if len(column) == 4 and column[0] != 'AppID' and column[2] != 'Name':
-            results[column[2]] = column[0]
-
-    # Print the sorted results
-    for name, appid in sorted(results.items()):
-        sys.stdout.buffer.write(f'{name} (AppID: {appid})\n'.encode('utf8'))
-
-    sys.stdout.buffer.flush()
 
 
 if __name__ == '__main__':
@@ -43,8 +16,30 @@ if __name__ == '__main__':
     parser.add_argument('search', help='find a game by name or app id', type=str)
     args = parser.parse_args()
 
-    # Try the search term using steamdb.info
+    # Perform the query using Requests
     try:
-        search_db(args.search)
-    except Exception as e:
-        print('Error: ', e)
+        url = 'https://steamdbasd.info/search/?a=app&q={}&type=1&category=0'.format(args.search)
+        r = requests.get(url, headers={'User-Agent': 'Mozilla 5.0/NT'})
+        r.raise_for_status()    # throws a Requests Exception on http error
+    except requests.RequestException as e:
+        print(e.strerror)
+        sys.exit(0)
+
+    # Parse html response using BeautifulSoup
+    soup = BeautifulSoup(r.text, 'html.parser')
+    table = soup.find('table', {'class': 'table table-bordered table-hover'})
+    if not table:
+        print('Found 0 results.')
+        sys.exit(0)
+
+    # Create a dict of games and app ids
+    results = {}
+    for row in table.find_all('tr'):
+        column = row.text.strip().split('\n')
+        if len(column) == 4 and column[0] != 'AppID' and column[2] != 'Name':
+            results[column[2]] = column[0]
+
+    # Print the results
+    for name, appid in sorted(results.items()):
+        sys.stdout.buffer.write('{} (AppID: {})\n'.format(name, appid).encode('utf8'))
+    sys.stdout.buffer.flush()
