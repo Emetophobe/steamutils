201e3e478ad3174273c75d9716efd9b26a6bf90d
Emetophobe <snapnaw@gmail.com>
2021-01-04 11:29:17 -0500

    Removed 'Skipping' messages.
    Minor code cleanup.

--- cleanfiles.py
+++ cleanfiles.py
@@ -12,54 +12,47 @@ from listgames import list_games
 def main():
     # Parse arguments
     parser = argparse.ArgumentParser(description='Clean up left over files and directories.',
-                                     epilog='All changes are final. USE AT YOUR OWN RISK!')
+                                     epilog='Warning: All file changes are final. USE AT YOUR OWN RISK!')
     parser.add_argument('steamdir', help='location of the steam directory', type=str)
     args = parser.parse_args()
 
-    # Get installed games
+    # Get list of installed games
     games = list_games(args.steamdir)
 
-    # Search for manifests without a matching installdir
-    issues = 0
+    # Check for manifest files without a matching installdir
     for game in games:
         if not os.path.exists(game['installdir']):
             delete_manifest(game)
-            issues += 1
 
-    # Search for directories without a matching manifest
+    # Check for installdirs without a matching manifest file
     steam_common = os.path.join(args.steamdir, 'steamapps', 'common')
-    for filename in os.listdir(steam_common):
-        path = os.path.join(steam_common, filename)
+    for path in os.listdir(steam_common):
+        path = os.path.join(steam_common, path)
         if os.path.isdir(path):
             for game in games:
                 if path == game['installdir']:
                     break
             else:
                 delete_directory(path)
-                issues += 1
 
-    if not issues:
-        print('Found 0 issues.')
+
+def confirm_delete(message):
+    """ Prompt user for a yes/no comfirmation before deleting. """
+    return input(message).lower() in ('yes', 'y')
 
 
 def delete_manifest(game):
-    """ Prompt to delete an unused manifest file without a matching game directory. """
-    print('\nThe directory for {} no longer exists: {}'.format(game['name'], game['installdir']))
-    result = input('Would you like to remove the unused manifest file? [Y/N] ').lower()
-    if result == 'yes' or result == 'y':
+    """ Delete a left over manifest file. """
+    print('\nThe directory for', game['name'], 'no longer exists:', game['installdir'])
+    if confirm_delete('Would you like to delete the left over manifest file? '):
         os.remove(game['manifest'])
-    else:
-        print('Skipping')
 
 
 def delete_directory(installdir):
-    """ Prompt to delete an uninstalled installdir without a matching manifest file. """
-    print('\nFound a directory without a matching manifest file: {}'.format(installdir))
-    result = input('Would you like to delete it? [Y/N] ').lower()
-    if result == 'yes' or result == 'y':
+    """ Delete a left over installdir. """
+    print('\nFound a directory without a matching manifest file:', installdir)
+    if confirm_delete('Would you like to delete the left over directory? '):
         shutil.rmtree(installdir)
-    else:
-        print('Skipping')
 
 
 if __name__ == '__main__':
